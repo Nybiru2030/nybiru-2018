0ac592c77f4c8c26d6e3bf8f02599c045c7b3d61
Bellizari <robson.bellizari@nybiru.com.br>
2019-03-13 19:16:04 -0300

    Alterção conteúdo tela Inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Robson Bellizari</h1>
+    <p>Nybiru Mobility MX.</p>
+    <p>versão 0.0</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Próxima Página</Link>
   </Layout>
 )
 
