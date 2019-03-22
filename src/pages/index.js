58db2668546d4886b1159f4e0359cc94797c2c56
Bellizari <robson.bellizari@nybiru.com.br>
2019-03-22 14:58:30 -0300

    Teste CVC
    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,14 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Robson Bellizari</h1>
-    <p>Nybiru Mobility MX.</p>
+    <SEO title="Teste" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Go REACTJS</h1>
+    <p>Hooks é Vida</p>
     <p>versão 0.0</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Próxima</Link>
+    <Link to="/page-2/">Próxima 1</Link>
   </Layout>
 )
 
