3a129d858e3890f6b5a3bcf00a62783893f9eb84
bellizari <robson.bellizari@nybiru.com.br>
2019-04-03 02:55:33 -0300

    teste
    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Próxima</Link>
+    <Link to="/page-2/">Próxima1</Link>
   </Layout>
 )
 
